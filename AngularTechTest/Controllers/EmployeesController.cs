e30ffcb8e8ebedb3430b5a01000c7ec84d63e082
mhdalkhbaz <mohamadalkhbaz98@gmail.com>
2022-02-17 07:53:38 +0200

    CreatAPP

---
+++ AngularTechTest/Controllers/EmployeesController.cs
@@ -0,0 +1,52 @@
+﻿using AngularTechTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularTechTest.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private CompanyDBEntities db = new CompanyDBEntities();
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: Employees
+        public JsonResult GetEmployeesJson()
+        {
+            var emp = db.Employees.ToList();
+
+            return new JsonResult { Data = emp, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+
+
+        public JsonResult GetEmployee(int id)
+        {
+            var emp = db.Employees.Find(id);
+
+            return new JsonResult { Data = emp, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+
+
+        [HttpPost]
+        public ActionResult SaveRecord([Bind(Include = "Id,Name,Department,Salary")] Employee employee)
+        {
+
+            db.Entry(employee).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("About");
+
+        }
+
+    }
+}
---
+++ AngularTechTest/Scripts/MyApp.js
@@ -0,0 +1,70 @@
+﻿// Code goes here
+
+var myApp = angular.module('MyApp', ['angularUtils.directives.dirPagination']);
+
+
+function OtherController($scope) {
+    $scope.pageChangeHandler = function (num) {
+        console.log('items page changed to ' + num);
+    };
+}
+
+
+
+myApp.controller("myCtrl", function ($scope, $http) {
+    $http.get("/Employees/GetEmployeesJson").then(function (res) {
+        $scope.currentPage = 1;
+        $scope.pageSize = 4;
+        $scope.items = [];
+
+        for (var i = 0; i < res.data.length; i++) {
+            var dish = res.data[i];
+            //  console.log( res.data[j].Salary);
+
+            $scope.items.push(dish);
+
+        }
+
+
+        $scope.editRecord = function (id) {
+            if (id > 0) {
+                $http.get("/Employees/GetEmployee/" + id)
+                    .then(function (response) {
+                        //console.log(response.data);
+                        $scope.Employee = response.data;
+
+                    });
+            }
+
+        }
+
+        $scope.updateRecord = function (updateSalary, id) {
+            $scope.datas = {};
+            $http.get("/Employees/GetEmployee/" + id)
+                .then(function (response) {
+                    $scope.Employee2 = response.data;
+
+                    if (updateSalary != response.data.Salary) {
+
+                        $scope.datas = response.data;
+                        $scope.datas.Salary = updateSalary;
+                        return $http({
+                            url: "/Employees/SaveRecord",
+                            method: "POST",
+                            data: $scope.datas
+                        })
+
+                    }
+                });
+            location.reload();
+
+        }
+
+
+
+
+
+    })
+
+
+})
